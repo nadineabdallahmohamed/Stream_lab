fbb3c5a0bcfe302f9c1cdc9b8b941509ce56b8fe
Nadine Abdallah <64757359+nadineabdallahmohamed@users.noreply.github.com>
2021-05-28 00:04:06 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Stream_lab;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+       String first_str = "Test example";
+       System.out.print(Stream.Test_str(first_str));
+	}
+
+}
